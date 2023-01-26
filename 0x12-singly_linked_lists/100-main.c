33f748bdfafc732b22f4a1f202f37723e4d5adfa
omeyennenna <nnennaphilomina490@gmail.com>
2023-01-26 14:59:31 -0800

    task 0-task 6

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("(tortoise have sense of a hare's nature, challenges it to race.)\n");
+return (0);
+}
